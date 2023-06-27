1f97ab8f6528b66f46cd0096702654724d5dd649
Ümit Yetişir <umitytsr@gmail.com>
2023-06-27 15:06:40 +0300

    add package data, MovieAdapter and build.gradle dependencies

---
+++ app/src/main/java/com/umitytsr/movieapp/ui/home/MovieAdapter.kt
@@ -0,0 +1,23 @@
+package com.umitytsr.movieapp.ui.home
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>(){
+
+    inner class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
