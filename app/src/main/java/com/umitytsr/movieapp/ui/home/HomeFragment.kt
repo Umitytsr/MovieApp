8b5dfaf3ec751726e362f1fe0d1f7239ea530223
Ümit Yetişir <umitytsr@gmail.com>
2023-06-27 14:39:12 +0300

    add item_row_movie.xml and desing

--- app/src/main/java/com/umitytsr/movieapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/umitytsr/movieapp/ui/home/HomeFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.umitytsr.movieapp.R
+import com.umitytsr.movieapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 }
