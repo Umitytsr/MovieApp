adae375201ea456335d5543bfedabff188fbaf61
Ümit Yetişir <umitytsr@gmail.com>
2023-06-28 19:04:21 +0300

    add Actor.kt and toActor in Extensions.kt

---
+++ app/src/main/java/com/umitytsr/movieapp/domain/model/Actor.kt
@@ -0,0 +1,12 @@
+package com.umitytsr.movieapp.domain.model
+
+data class Actor(
+    val backdropPath: String?,
+    val firstAirDate: String?,
+    val genreİds: List<Int>?,
+    val id: Int?,
+    val name: String?,
+    val overview: String?,
+    val posterPath: String?,
+    val voteAverage: Double?,
+)
