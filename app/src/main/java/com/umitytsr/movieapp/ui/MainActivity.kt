bd2db294bfcb43e1d7da33b7d414ebb6f4299140
Ümit Yetişir <umitytsr@gmail.com>
2023-06-29 14:10:21 +0300

    add bottom navigation

--- app/src/main/java/com/umitytsr/movieapp/ui/MainActivity.kt
+++ app/src/main/java/com/umitytsr/movieapp/ui/MainActivity.kt
@@ -2,13 +2,22 @@ package com.umitytsr.movieapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.umitytsr.movieapp.R
+import com.umitytsr.movieapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        setupWithNavController(binding.bottomView,navHostFragment.navController)
     }
 }
