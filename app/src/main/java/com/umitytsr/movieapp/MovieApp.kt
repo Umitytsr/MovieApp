d5f4615bc565a76aae5733d1b46406fbb5c280ac
Ümit Yetişir <umitytsr@gmail.com>
2023-07-17 09:48:19 +0300

    add Timber

--- app/src/main/java/com/umitytsr/movieapp/MovieApp.kt
+++ app/src/main/java/com/umitytsr/movieapp/MovieApp.kt
@@ -1,8 +1,24 @@
 package com.umitytsr.movieapp
 
 import android.app.Application
+import com.google.firebase.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MovieApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(object : Timber.Tree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                }
+            })
+        }
+
+        Timber.plant(Timber.DebugTree())
+    }
 }
